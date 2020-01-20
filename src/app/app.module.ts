62958ccd58e6b58b2907e9e1b253d90e409a667e
Jack Woodrow <me@jackwoodrow.io>
2020-01-20 15:38:18 -0500

    Assignment 1 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentCardComponent } from './content-card/content-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentCardComponent
   ],
   imports: [
     BrowserModule,
