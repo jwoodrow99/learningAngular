c06fb4628d5edc4ab107ed6642fb5198c3ac6a37
Jack Woodrow <me@jackwoodrow.io>
2020-03-13 13:42:30 -0400

    finished 7

--- src/app/services/content.service.ts
+++ src/app/services/content.service.ts
@@ -3,22 +3,35 @@ import {contentList} from '../contentDb';
 import {Content} from '../content-card/content-card-helper';
 import { Observable, of } from 'rxjs';
 import { MessageService } from '../message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContentService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(private messageService: MessageService, private http: HttpClient) { }
 
 
     getContentObs(): Observable<Content[]> {
         this.messageService.add('Content Retrieved');
-        return of(contentList);
+        return this.http.get<Content[]>("api/content");
     }
 
     addContentObs(): void {
         this.messageService.add('Content Added!');
         return;
     }
+
+    private httpOptions = {
+        headers: new HttpHeaders({ 'Content-type': 'application/json' })
+    };
+
+    addContent(content: Content): Observable<Content>{
+        return this.http.post<Content>("api/content", content, this.httpOptions);
+    }
+
+    updateContent(content: Content): Observable<any>{
+        return this.http.put("api/content", content, this.httpOptions);
+    }   
 }
